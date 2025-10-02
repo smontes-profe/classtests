f1dd5ef599beaa4ac8366a517452897daa9aad6e
Sergio Montes <smontes@generagames.com>
2025-10-02 21:04:05 +0200

    created the "plantilla folder"

---
+++ plantilla/script.js
@@ -0,0 +1,5 @@
+// Función simple para manipular el DOM
+function mostrarAlerta() {
+  alert("¡Hola desde JavaScript!");
+  document.getElementById("mensaje").innerText = "El texto ha cambiado gracias a JS 🚀";
+}
